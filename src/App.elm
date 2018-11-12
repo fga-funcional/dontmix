947fb402988fbdf1e4f8b9c346d05745d5f95d29
Matheus Bernardo <matheussber@aluno.unb.br>
2018-11-12 16:01:21 -0200

    Creating search actions
    Co-authored-by: MatheusRich <matheusrichardt@gmail.com>

--- src/App.elm
+++ src/App.elm
@@ -4,13 +4,15 @@ import Browser
 import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.Events exposing (..)
+import Http
 import SpotifyDecoder as D
 
 
 main =
-    Browser.sandbox
+    Browser.element
         { view = view
         , update = update
+        , subscriptions = subscriptions
         , init = init
         }
 
@@ -30,17 +32,27 @@ type alias Music =
 type alias Model =
     { musics : List Music
     , input : String
+    , searchedMusics : List Music
     }
 
 
-init : Model
-init =
-    { musics =
-        [ { name = "Music 1", artist = "", album = "", duration = 0 }
-        , { name = "Music 2", artist = "", album = "", duration = 0 }
-        ]
-    , input = ""
-    }
+init : () -> ( Model, Cmd Msg )
+init _ =
+    ( { musics = []
+      , input = ""
+      , searchedMusics = []
+      }
+    , Cmd.none
+    )
+
+
+
+-- SUBSCRIPTIONS -----------------------------------------
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Sub.none
 
 
 
@@ -50,16 +62,31 @@ init =
 type Msg
     = Add (List Music)
     | Save String
+    | SaveSearchedMusic (Result Http.Error (List Music))
+    | Search
 
 
-update : Msg -> Model -> Model
+update : Msg -> Model -> ( Model, Cmd Msg )
 update msg m =
     case msg of
         Add musics ->
-            { m | musics = musics ++ m.musics }
+            ( { m | musics = musics ++ m.musics }, Cmd.none )
 
         Save input ->
-            { m | input = input }
+            ( { m | input = input }, Cmd.none )
+
+        Search ->
+            ( m, searchMusic m.input )
+
+        SaveSearchedMusic result ->
+            case result of
+                Ok searchedMusics ->
+                    ( { m | searchedMusics = searchedMusics }, Cmd.none )
+
+                Err _ ->
+                    ( {m | searchedMusics = []}
+                    , Cmd.none
+                    )
 
 
 
@@ -69,9 +96,10 @@ update msg m =
 view : Model -> Html Msg
 view m =
     div []
-        [ showPlaylist m.musics
+        [ showSavedMusics m.musics
         , input [ placeholder "Type here", onInput Save ] []
-        , button [ onClick (Add D.decode) ] [ text "Add Music" ]
+        , button [ onClick <| Search ] [ text "Search Music" ]
+        , showPlaylist m.searchedMusics
         ]
 
 
@@ -79,7 +107,7 @@ showPlaylist : List Music -> Html Msg
 showPlaylist playlist =
     let
         showMusic music =
-            li []
+            li [ onClick <| Add [ music ], style "cursor" "pointer" ]
                 [ h1 []
                     [ text music.name
                     , text music.artist
@@ -92,3 +120,52 @@ showPlaylist playlist =
             List.map showMusic playlist
     in
     ul [] musics
+
+
+showSavedMusics : List Music -> Html Msg
+showSavedMusics playlist =
+    let
+        showMusic music =
+            li []
+                [ p []
+                    [ b [] [text "Song: "] ,text music.name ]
+                , p []
+                    [ b [] [text "Artist: "] , text music.artist ]
+                , p []
+                    [  b [] [text "Album: "] , text music.album ]
+                , p []
+                    [  b [] [text "Duration (ms): "] , text (String.fromInt music.duration) ]
+                ]
+
+        musics =
+            List.map showMusic playlist
+    in
+    ol [] musics
+
+
+searchMusic : String -> Cmd Msg
+searchMusic query =
+    Http.send SaveSearchedMusic (searchRequest query)
+
+
+searchRequest : String -> Http.Request (List Music)
+searchRequest query =
+    let
+        headers =
+            [ Http.header "Authorization" "Bearer BQATZ5YTV6cG6158GTSOFFI2FstKEtvpfbZzMPGU1EVKX1yr0VJARB62rp0NwqY4hn9aAn7wYA1v4vYKt2pXSZoQAOZI3XaCqkYvE07um88eK4z14yScxuvrC_t10eyKlKIGnUXCXeTPAc6X2sM"
+            , Http.header "Accept" "application/json"
+            , Http.header "Content-Type" "application/json"
+            ]
+
+        url =
+            "https://api.spotify.com/v1/search?type=track&limit=5&q=" ++ query
+    in
+    Http.request
+        { method = "GET"
+        , headers = headers
+        , url = url
+        , body = Http.emptyBody
+        , expect = Http.expectJson D.musicsDecoder
+        , timeout = Nothing
+        , withCredentials = False
+        }
