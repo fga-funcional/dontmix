7699ea774a9b7efcac3febccac7f84f701160201
Matheus Bernardo <matheussber@aluno.unb.br>
2018-12-04 00:41:05 -0200

    Adding get and put routes to create page for dontmix

--- api/app/Main.hs
+++ api/app/Main.hs
@@ -2,27 +2,44 @@
 module Main where
 
 import Auth (getAccessToken)
+import Database (findOrCreatePageJSON, editPageJSON)
 import Web.Scotty as Scotty
 import Network.Wreq as Wreq
 import Control.Lens
 import Data.Aeson.Lens (_String, key, nth)
 import Control.Monad.IO.Class (liftIO)
 import Network.Wai.Middleware.Cors
+import Data.ByteString.Lazy.Char8 as Char8
 
 main :: IO ()
 main = scotty 3000 $ do
-    middleware simpleCors    
-    Scotty.get "/search/:query" $ do
-        query <- Scotty.param "query"
-        searchMusics query
+    middleware simpleCors
     
-    Scotty.get "/recommendation/:query" $ do
-        query <- Scotty.param "query"
-        getRecommendation query
+    Scotty.get "/:page"  findOrCreatePage
 
-        
-searchMusics :: String -> ActionM()
-searchMusics query = do
+    Scotty.put "/:page" editPage
+
+    Scotty.get "/search/:query" searchMusics
+    
+    Scotty.get "/recommendation/:query" getRecommendation 
+
+findOrCreatePage :: ActionM()
+findOrCreatePage = do
+    pageName <- Scotty.param "page"
+    filePath <- liftIO $ findOrCreatePageJSON pageName
+    file filePath
+
+editPage :: ActionM()
+editPage = do
+    page <- Scotty.param "page"
+    body <- Scotty.body
+
+    filePath <- liftIO $ editPageJSON page (Char8.unpack body)
+    file filePath
+
+searchMusics :: ActionM()
+searchMusics = do
+    query <- Scotty.param "query"
     let url =  "https://api.spotify.com/v1/search?type=track&limit=5&q=" ++ query
 
     access_token <- liftIO $ getAccessToken
@@ -31,8 +48,9 @@ searchMusics query = do
     r <-  liftIO $ Wreq.getWith opts url
     raw  (r ^. responseBody) 
 
-getRecommendation :: String -> ActionM()
-getRecommendation query = do
+getRecommendation :: ActionM()
+getRecommendation = do
+    query <- Scotty.param "query"
     let url =  "https://api.spotify.com/v1/recommendations?seed_tracks=" ++ query
 
     access_token <- liftIO $ getAccessToken
---
+++ api/src/Database.hs
@@ -0,0 +1,27 @@
+module Database
+    ( findOrCreatePageJSON,
+      editPageJSON
+    ) where
+
+import System.IO
+import System.Directory
+
+findOrCreatePageJSON :: FilePath -> IO String
+findOrCreatePageJSON path = do
+    let filePath = "./dontmixdb/" ++ path ++ ".json"
+
+    createDirectoryIfMissing False  "./dontmixdb/"
+    h <- openFile filePath ReadWriteMode
+    raw_contents <- hGetContents h
+    startJSONWithDefault filePath raw_contents
+    return filePath
+    
+startJSONWithDefault :: FilePath -> String -> IO ()
+startJSONWithDefault path "" = writeFile path "{ \"Selected\": {} \"Recommended\": {} }" 
+startJSONWithDefault path _ = mempty
+
+editPageJSON :: FilePath -> String -> IO String
+editPageJSON path contents = do
+    let filePath = "./dontmixdb/" ++ path ++ ".json"
+    writeFile filePath contents
+    return filePath
