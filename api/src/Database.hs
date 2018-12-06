a25696fd83f0c7e5de7faff3b207eb116a3a277f
Matheus Bernardo <matheussber@aluno.unb.br>
2018-12-06 00:22:53 -0200

    Using strict IO to fix weird bugs

--- api/src/Database.hs
+++ api/src/Database.hs
@@ -1,23 +1,24 @@
 module Database
-    ( findOrCreatePageJSON,
-      editPageJSON
-    ) where
+    ( findOrCreatePageJSON
+    , editPageJSON
+    )
+where
 
-import System.IO
-import System.Directory
+import qualified System.IO.Strict              as Strict
+import           System.Directory
 
 findOrCreatePageJSON :: FilePath -> IO String
 findOrCreatePageJSON path = do
     let filePath = "./db/" ++ path ++ ".json"
 
-    createDirectoryIfMissing False  "./db/"
-    h <- openFile filePath ReadWriteMode
-    raw_contents <- hGetContents h
-    startJSONWithDefault filePath raw_contents
+    createDirectoryIfMissing False "./db/"
+    contents <- Strict.readFile filePath
+    startJSONWithDefault filePath contents
     return filePath
-    
+
 startJSONWithDefault :: FilePath -> String -> IO ()
-startJSONWithDefault path "" = writeFile path "{ \"Selected\": [], \"Recommended\": [] }" 
+startJSONWithDefault path "" =
+    writeFile path "{ \"Selected\": [], \"Recommended\": [] }"
 startJSONWithDefault path _ = mempty
 
 editPageJSON :: FilePath -> String -> IO String
