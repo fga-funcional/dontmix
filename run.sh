197bb67616eadc92285228b48341aecfa7b78dc0
Matheus Richard <matheusrichardt@gmail.com>
2018-12-10 17:21:46 -0200

    Adding script to run project

--- run.sh
+++ run.sh
@@ -1,2 +1,12 @@
 #!/usr/bin/env bash
+# Running API
+cd api/
+echo "main" | stack ghci --allow-different-user &
+
+# Running Front-end
+cd ../dontmix/
+elm-live src/App.elm --pushstate &
+
+# Running DEVD
+cd ..
 devd -X /api/=http://localhost:3000 /=http://localhost:8000 -p 8080
